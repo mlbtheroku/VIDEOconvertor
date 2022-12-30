505f44bd8f10014a0b0317e7f18e6844f8fbca8a
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-12-30 18:02:38 +0630

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,13 +9,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = config("API_ID", 15830858, cast=int)
 API_HASH = config("API_HASH", "2c015c994c57b312708fecc8a2a0f1a6")
-BOT_TOKEN = config("BOT_TOKEN", "5833550185:AAFzUdwF-uiFxIVwzrVS7VepS5GH9tfv0Bk")
-BOT_UN = config("BOT_UN", "AIOTest_VBot")
+BOT_TOKEN = config("BOT_TOKEN", "5410658090:AAFiwio1AMM7hS9FZShr1Evmjw59lA2lRMo")
+BOT_UN = config("BOT_UN", "AIO_Video_Tools_Bot")
 AUTH_USERS = config("AUTH_USERS", 5468192421, cast=int)
-LOG_CHANNEL = config("LOG_CHANNEL", "aiologk")
+LOG_CHANNEL = config("LOG_CHANNEL", "AIOBOTLOG")
 LOG_ID = config("LOG_ID", "-1001749073420")
 FORCESUB = config("FORCESUB", "-1001767363693")
-FORCESUB_UN = config("FORCESUB_UN", "fsubcc")
+FORCESUB_UN = config("FORCESUB_UN", "subtouse")
 ACCESS_CHANNEL = config("ACCESS_CHANNEL", "-1001631866019")
 MONGODB_URI = config("MONGODB_URI", "mongodb+srv://aio:aio@aio.5z4gxok.mongodb.net/?retryWrites=true&w=majority")
 
