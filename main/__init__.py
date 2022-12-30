cd376ae0dca507b697237eba1b0c738c7d747748
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-12-30 16:14:09 +0630

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,10 +13,10 @@ BOT_TOKEN = config("BOT_TOKEN", "5833550185:AAFzUdwF-uiFxIVwzrVS7VepS5GH9tfv0Bk"
 BOT_UN = config("BOT_UN", "AIOTest_VBot")
 AUTH_USERS = config("AUTH_USERS", 5468192421, cast=int)
 LOG_CHANNEL = config("LOG_CHANNEL", "aiologk")
-LOG_ID = config("LOG_ID", -1001749073420)
-FORCESUB = config("FORCESUB", -1001767363693)
+LOG_ID = config("LOG_ID", "-1001749073420")
+FORCESUB = config("FORCESUB", "-1001767363693")
 FORCESUB_UN = config("FORCESUB_UN", "fsubcc")
-ACCESS_CHANNEL = config("ACCESS_CHANNEL", -1001631866019)
+ACCESS_CHANNEL = config("ACCESS_CHANNEL", "-1001631866019")
 MONGODB_URI = config("MONGODB_URI", "mongodb+srv://aio:aio@aio.5z4gxok.mongodb.net/?retryWrites=true&w=majority")
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
